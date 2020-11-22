94175257100e421c8a85c8d52345c65e9293bca0
soreak <1498985670@qq.com>
2020-11-22 14:45:35 +0800

    first commit

---
+++ src/main/java/com/soreak/welog/WelogApplication.java
@@ -0,0 +1,13 @@
+package com.soreak.welog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WelogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WelogApplication.class, args);
+    }
+
+}

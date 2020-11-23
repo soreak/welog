09bd263bbd647218f9e4a27e4005cc112a804c9f
soreak <1498985670@qq.com>
2020-11-23 17:06:02 +0800

    2020.11.23:编写了日志处理，前端页面的整合，实体类的创建

--- src/main/java/com/soreak/web/IndexController.java
+++ src/main/java/com/soreak/web/IndexController.java
@@ -3,18 +3,32 @@ package com.soreak.web;
 import com.soreak.NotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class IndexController {
 
     @GetMapping("/")
     public String index(){
-        int a = 1/0;
-        System.out.println(a);
-        String b = null;
-        if (b == null ){
-            throw  new NotFoundException("no");
-        }
+//        int a = 1/0;
+//        System.out.println(a);
+//        String b = null;
+//        if (b == null ){
+//            throw  new NotFoundException("no");
+//        }
+
         return "index";
+    }
+
+    @GetMapping("/blog")
+    public String blog(){
+//        int a = 1/0;
+//        System.out.println(a);
+//        String b = null;
+//        if (b == null ){
+//            throw  new NotFoundException("no");
+//        }
+
+        return "blog";
     }
 }

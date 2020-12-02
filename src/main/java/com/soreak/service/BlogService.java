023bb06e3087e1c77ae538091efc59c898e607db
soreak <1498985670@qq.com>
2020-12-02 15:45:48 +0800

    2020.12.02: 博客分类页  完成

--- src/main/java/com/soreak/service/BlogService.java
+++ src/main/java/com/soreak/service/BlogService.java
@@ -16,6 +16,9 @@ public interface BlogService {
     Page<Blog> listBlog(Pageable pageable, BlogQuery blog);
 
     Page<Blog> listBlog(Pageable pageable);
+
+    Page<Blog> listBlog(Long tagId,Pageable pageable);
+
     Page<Blog> listBlog(String query,Pageable pageable);
 
     Blog saveBlog(Blog blog);
